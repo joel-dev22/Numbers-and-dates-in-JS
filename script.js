6a7a38604ef1c30157e4d580b232c194be8c5346
joel-dev22 <joeltmathew1999@gmail.com>
2022-10-17 16:13:55 +0530

    learned about checking and converting numbers in JS

--- script.js
+++ script.js
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -251,3 +251,17 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+console.log(23 === 23.0);
+console.log(0.1 + 0.2 === 0.3);
+
+console.log(typeof Number('23'));
+console.log(typeof +'77');
+
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseFloat('30.22px', 10));
+// NaN - Check value is NaN
+console.log(Number.isNaN(+'40s'));
+
+// Check if value is a number
+console.log(Number.isFinite(22 / 0));
+console.log(Number.isFinite(22 / 2));
